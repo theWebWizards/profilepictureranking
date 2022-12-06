dff36973d5eb3b7970ef41566f2c287e08e90028
Justin Khan <justinkhan707@gmail.com>
2022-12-06 02:09:21 +0000

    changes in config.py

--- App/config.py
+++ App/config.py
@@ -1,4 +1,4 @@
-SQLALCHEMY_DATABASE_URI = "sqlite:///temp-database.db"
+SQLALCHEMY_DATABASE_URI = "postgresql://profilepicrankingsql_user:q0wtHrsvgDBDXHfpE6nAHvaaHVjquOSr@dpg-ce79uppa6gdnvfsknjeg-a/profilepicrankingsql"
 SECRET_KEY = "secret key"
 JWT_EXPIRATION_DELTA = 7
-ENV = "DEVELOPMENT"
+ENV = "PRODUCTION"
